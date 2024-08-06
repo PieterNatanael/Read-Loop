a3ea059730ebd2bfbea7cb58eb27bbae1f2a4fd2
PieterNatanael <pieternatanael@icloud.com>
2024-08-06 10:15:12 +0800

    Initial Commit

---
+++ Read Loop/Read_LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Read_LoopApp.swift
+//  Read Loop
+//
+//  Created by Pieter Yoshua Natanael on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Read_LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
